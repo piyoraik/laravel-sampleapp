bc54d9ed218328c5eecfa5824414b9a3f01f1978
raityupiyo <42093052+piyoraik@users.noreply.github.com>
2020-10-07 17:30:12 +0900

    first commit

---
+++ app/Http/Controllers/ListingsController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Listing;
+use Auth;
+use Validator;
+
+use Illuminate\Http\Request;
+
+class ListingsController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth')
+    }
+
+    public function index()
+    {
+        $listings = Listing::where('user_id', Auth::user()-id)
+            ->orderBy('created_at', 'asc')
+            ->get();
+
+        return view('listing/index', ['listings' => $listings])
+    }
+
+    public function new()
+    {
+        return view('listing/new');
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), ['list_name' => 'required|max:255']);
+
+        if($validator->fails())
+        {
+            return redirect()->back()->withErrors($validator->errors())->withinput();
+        }
+
+        $listings = new Listing;
+        $listings->title = $request->list_name;
+        $listings->user_id = Auth:::user()->id;
+
+        $listings->save();
+
+        return redirect('/');
+    }
+
+    public function edit($listing_id)
+    {
+        $listing = Listing::find($listing_id);
+        return view('listing/edit',['listing' => $listing]);
+    }
+
+    public function update(Request $request)
+    {
+        $validator = Validator::make($request->all(), ['list_name' => 'required|max:255']);
+
+        if($validator->fails())
+        {
+            return redirect()->back()->withErrors($validator->errors())->withinput();
+        }
+
+        $listing = Listing::find($request->id);
+        $listing->title = $request->list_name;
+        $listing->save();
+        return redirect('/');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', 'ListingsController@index');
+
+Route::get('/new', 'ListingsController@new')->name('new');
+
+Route::post('/listings','ListingsController@store');
+
+Route::get('/listingsedit/{listing_id}',"ListingsController@edit");
+
+Route::post('/listing/edit','ListingsController@update');
+
+Route::get('/listingsdelete/{listing_id}','ListingsController@destroy');
